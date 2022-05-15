a9ba30fa2114a1c7effc5725cdc2250375b823e8
misty-spring <105533011+misty-spring@users.noreply.github.com>
2022-05-15 17:56:55 -0400

    Update ContentPackData.cs
    Added translation support

--- SpousesIsland/Framework/ContentPackData.cs
+++ SpousesIsland/Framework/ContentPackData.cs
@@ -1,17 +1,17 @@
-﻿//using StardewModdingAPI;
+using System.Collections.Generic;
 
 namespace SpousesIsland.Framework
 {
-    public class ContentPackData //: ITranslationHelper
+    public class ContentPackData
     {
-        //add support for translations in the future
-
+    
         public string Spousename { get; set; }
         public string ArrivalPosition { get; set; }
         public string ArrivalDialogue { get; set; }
         public Location1Data Location1 { get; set; }
         public Location2Data Location2 { get; set; }
         public Location3Data Location3 { get; set; }
+        public List<DialogueTranslation> Translations { get; set; } = new List<DialogueTranslation>();
 
         public ContentPackData()
         {
@@ -22,23 +22,22 @@ namespace SpousesIsland.Framework
             ArrivalPosition = cpd.ArrivalPosition;
             ArrivalDialogue = cpd.ArrivalDialogue;
 
-            //Location1 = cpd.Location1;
             Location1.Name = cpd.Location1.Name;
             Location1.Time = cpd.Location1.Time;
             Location1.Position = cpd.Location1.Position;
             Location1.Dialogue = cpd.Location1.Dialogue;
 
-            //Location2 = cpd.Location2;
             Location2.Name = cpd.Location2.Name;
             Location2.Time = cpd.Location2.Time;
             Location2.Position = cpd.Location2.Position;
             Location2.Dialogue = cpd.Location2.Dialogue;
 
-            //Location3 = cpd.Location3;
             Location3.Name = cpd.Location3.Name;
             Location3.Time = cpd.Location3.Time;
             Location3.Position = cpd.Location3.Position;
             Location3.Dialogue = cpd.Location3.Dialogue;
+            
+            Translations = cpd.Translations;
         }
     }
 }
