964588264765e4c69f0d1ba82236f3706059fe97
misty-spring <105533011+misty-spring@users.noreply.github.com>
2022-05-29 06:15:40 -0400

    Update SGIValues.cs

--- SpousesIsland/SGIValues.cs
+++ SpousesIsland/SGIValues.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework;
 using StardewValley;
 using StardewValley.Objects;
 using System;
@@ -8,7 +8,6 @@ namespace SpousesIsland
 {
     internal class SGIValues
     {
-        //check if custom schedule is for already added spouse
         internal static bool CheckSpouseName(string spouse)
         {
             switch (spouse)
@@ -28,51 +27,16 @@ namespace SpousesIsland
                 case "Shane":
                 case "Claire":
                 case "Lance":
-                case "Magnus":
                 case "Olivia":
                 case "Sophia":
                 case "Victor":
+                case "Wizard":
                     return true;
                 default:
                     return false;
             }
         }
 
-        //check if vanilla spouse is in married list
-        private List<string> Spouses = new List<string>();
-        internal bool IsMarriedToMain(string c)
-        {
-            if (Game1.MasterPlayer.spouse != null && Game1.MasterPlayer.spouse.Contains(c))
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        internal void SpousesList()
-        {
-            foreach (string c in Game1.NPCGiftTastes.Keys)
-            {
-                if (IsMarriedToMain(c))
-                {
-                    Spouses.Add(c);
-                }
-            }
-        }
-
-        internal bool CheckIfMarried(string npc)
-        {
-            if (Spouses.Contains(npc))
-            {
-                return true;
-            }
-            else
-                return false;
-        }
-
-        //spouse bed
         GameLocation ifh = Game1.getLocationFromName("IslandFarmHouse");
         public Point getBedSpot(BedFurniture.BedType bed_type = BedFurniture.BedType.Any)
         {
@@ -129,9 +93,109 @@ namespace SpousesIsland
             }
         }
 
-        public static bool IsSpanish()
+        public static List<string> SpousesAddedByMod()
         {
-            return StardewValley.LocalizedContentManager.CurrentLanguageCode == LocalizedContentManager.LanguageCode.es;
+            List<string> SpousesAddedByMod = new ();
+            SpousesAddedByMod.Add("Abigail");
+            SpousesAddedByMod.Add("Alex");
+            SpousesAddedByMod.Add("Emily");
+            SpousesAddedByMod.Add("Elliott");
+            SpousesAddedByMod.Add("Haley");
+            SpousesAddedByMod.Add("Harvey");
+            SpousesAddedByMod.Add("Krobus");
+            SpousesAddedByMod.Add("Leah");
+            SpousesAddedByMod.Add("Maru");
+            SpousesAddedByMod.Add("Penny");
+            SpousesAddedByMod.Add("Sam");
+            SpousesAddedByMod.Add("Sebastian");
+            SpousesAddedByMod.Add("Shane");
+            SpousesAddedByMod.Add("Claire");
+            SpousesAddedByMod.Add("Lance");
+            SpousesAddedByMod.Add("Olivia");
+            SpousesAddedByMod.Add("Sophia");
+            SpousesAddedByMod.Add("Victor");
+            SpousesAddedByMod.Add("Wizard");
+            return SpousesAddedByMod;
+        }
+        internal static string RandomMap_nPos(Random ran, string spousename, bool ModInstalled, bool ActivatedConfig)
+        {
+            int choice = ran.Next(1, 2);
+            if (choice is 1 || ModInstalled is false || ActivatedConfig is false)
+            {
+                string result = spousename switch
+                {
+                    "Abigail" => "IslandWest 62 84 2",
+                    "Alex" => "IslandWest 69 77 2 alex_football",
+                    "Elliott" => "IslandNorth 19 15 0 \"Strings\\schedules\\Elliott:marriage_loc3\"",
+                    "Emily" => "IslandWestCave1 3 6 1 \"Strings\\schedules\\Emily:marriage_loc3\"",
+                    "Haley" => "IslandWest 76 12 2 haley_photo \"Strings\\schedules\\Haley:marriage_loc3\"",
+                    "Harvey" => "IslandWest 88 14 2 harvey_read \"Strings\\schedules\\Harvey:marriage_loc3\"",
+                    "Krobus" => "IslandFarmCave 2 6 2",
+                    "Leah" => "IslandWest 89 72 2 leah_draw \"Strings\\schedules\\Leah:marriage_loc3\"",
+                    "Maru" => "IslandFieldOffice 7 8 0 \"Strings\\schedules\\Maru:marriage_loc3\"",
+                    "Penny" => "IslandFieldOffice 2 7 2 \"Strings\\schedules\\Penny:marriage_loc3\"",
+                    "Sam" => "IslandSouthEast 23 14 2 \"Strings\\schedules\\Sam:marriage_loc3\"",
+                    "Sebastian" => "IslandNorth 40 23 2 \"Strings\\schedules\\Sebastian:marriage_loc3\"",
+                    "Shane" => "IslandSouthEastCave 29 6 2 \"Strings\\schedules\\Shane:marriage_loc3\"",
+                    "Claire" => "IslandWest 87 78 2",
+                    "Lance" => "IslandSouthEast 21 28 2 \"Characters\\Dialogue\\Lance:marriage_loc3\"",
+                    "Magnus" => "Caldera 22 23 0 \"Characters\\Dialogue\\Wizard:marriage_loc3\"",
+                    "Olivia" => "IslandNorth 36 73 0 \"Characters\\Dialogue\\Olivia:marriage_loc3\"",
+                    "Sophia" => "IslandFarmHouse 18 12 Sophia_Read \"Characters\\Dialogue\\Sophia:marriage_loc3\"",
+                    "Victor" => "IslandFarmHouse 19 5 2 Victor_Book2 \"Characters\\Dialogue\\Victor:marriage_loc3\"",
+                    _ => "IslandFarmHouse 5 5"
+                };
+                return result;
+            }
+            else
+            {
+                string MapName;
+                int RandomM = ran.Next(1, 8);
+                switch (RandomM)
+                {
+                    case 1:
+                        MapName = "Custom_GiCave";
+                        break;
+                    case 2:
+                        MapName = "Custom_GiForest";
+                        break;
+                    case 3:
+                        MapName = "Custom_GiRiver";
+                        break;
+                    case 4:
+                        MapName = "Custom_GiClearance";
+                        break;
+                    case 5:
+                        MapName = "Custom_IslandSW";
+                        break;
+                    case 6:
+                        MapName = "Custom_GiHut";
+                        break;
+                    case 7:
+                        MapName = "Custom_GiForestEnd";
+                        break;
+                    case 8:
+                        MapName = "Custom_GiRBeach";
+                        break;
+                    default:
+                        MapName = null;
+                        break;
+                }
+                Point Position = MapName switch
+                {
+                    "Custom_GiCave" => new Point(ran.Next(9, 22), ran.Next(10, 16)),
+                    "Custom_GiForest" => new Point(ran.Next(11, 29), ran.Next(21, 31)),
+                    "Custom_GiRiver" => new Point(ran.Next(15, 34), ran.Next(6, 11)),
+                    "Custom_GiClearance" => new Point(ran.Next(11, 22), ran.Next(13, 26)),
+                    "Custom_IslandSW" => new Point(ran.Next(10, 37), ran.Next(17, 24)),
+                    "Custom_GiHut" => new Point(ran.Next(1, 7), ran.Next(6, 8)),
+                    "Custom_GiForestEnd" => new Point(ran.Next(9, 25), ran.Next(25, 31)),
+                    "Custom_GiRBeach" => new Point(ran.Next(27, 35), ran.Next(6, 23)),
+                    _ => new Point(0, 0)
+                };
+                string result = MapName + Position.ToString();
+                return result;
+            }
         }
     }
 }
